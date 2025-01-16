ce1233c5eb040f8f3cac44b6f8487549042b1a2b
Pierre Gourseaud <pierre.gourseaud@gmail.com>
2025-01-16 16:44:10 +0100

    feat: compute expected_winrate

--- tests/units/elo_test.py
+++ tests/units/elo_test.py
@@ -1,6 +1,6 @@
 import pytest
 
-from tennis_predictor.helpers.elo import update_elo
+from tennis_predictor.helpers.elo import update_elo, compute_expected_winrate
 
 def test_update_elo():
     elo1 = 1500
@@ -8,3 +8,9 @@ def test_update_elo():
     elo1_new, elo2_new = update_elo(elo1, elo2)
     assert elo1_new == 1516
     assert elo2_new == 1484
+
+def test_compute_expected_winrate():
+    elo1 = 1500
+    elo2 = 1600
+    E_winrate = compute_expected_winrate(elo1, elo2)
+    assert E_winrate == pytest.approx(0.36, 0.01)
