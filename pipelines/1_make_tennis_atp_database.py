033133f744afb2c647752714b9cc50707c742b14
Pierre Gourseaud <pierre.gourseaud@gmail.com>
2025-01-16 18:08:29 +0100

    style: parametrize

--- pipelines/1_make_tennis_atp_database.py
+++ pipelines/1_make_tennis_atp_database.py
@@ -2,15 +2,21 @@ import os
 import subprocess
 import shutil
 
+PATH_TENNIS_ATP = './data/bronze/tennis_atp/'
+PATH_DB_SCRIPT = './examples/SQLite/convert_sqlite.sh'
+PATH_BACKTRACK = '../../..'
+DB_NAME = 'atpdatabase.db'
+PATH_OUTPUT = './data/silver/tennis_atp/'
+
 def move_and_run_script():
     # Change directory
-    os.chdir('./data/bronze/tennis_atp/')
-    
+    os.chdir(PATH_TENNIS_ATP)
     # Run the shell script
-    subprocess.run(['./examples/SQLite/convert_sqlite.sh'], check=True)
-    
+    subprocess.run([PATH_DB_SCRIPT], check=True)
+    # Change directory
+    os.chdir(PATH_BACKTRACK)
     # Move the database file
-    shutil.move('atpdatabase.db', '../../silver/tennis_atp/atpdatabase.db')
+    shutil.move(PATH_TENNIS_ATP + DB_NAME, PATH_OUTPUT + DB_NAME)
 
 if __name__ == "__main__":
     move_and_run_script()
