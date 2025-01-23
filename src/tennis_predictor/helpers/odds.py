207561750d0639da0b0521d2bd9b463b112549d6
Pierre Gourseaud <pierre.gourseaud@gmail.com>
2025-01-23 15:12:20 +0100

    feat: make shortnames

---
+++ src/tennis_predictor/helpers/odds.py
@@ -0,0 +1,49 @@
+import pandas as pd
+
+def _abbreviate_name(firstname: str, n_chars: int = 1) -> str:
+    """Lee Sin -> L.S."""
+    firstnames = firstname.split(' ')
+    initials = [firstnames[0][:n_chars]] + [s[0] for s in firstnames[1:] if len(s) > 0]
+    return '.'.join(initials) + '.'
+
+def make_shortname(df: pd.DataFrame) -> pd.DataFrame:
+    """Add a column of unique shortNames for players. Used to join dataframes "players" and "odds".
+        'Danilo', 'Acosta' => 'Acosta Dan.'
+        'Dario', 'Acosta' => 'Acosta Dar.'
+        'Diego', 'Acosta' => 'Acosta Di.'
+    """
+    # First pass with 1 char
+    n_chars = 1
+    df['shortName'] = (
+        df['lastName'] 
+        + ' ' 
+        + df['firstName'].apply(_abbreviate_name, args=[n_chars])
+    )
+    # Check duplicates
+    df_count = df.groupby('shortName')['id'].count()
+    df_duplicate_shortnames = df_count[df_count > 1]
+
+    # As long as there are duplicates, make longer shortnames for the duplicates
+    while df_duplicate_shortnames.size > 0:
+        # Some players can have the same names, stop at 10 chars.
+        if n_chars > 10:
+            break
+        n_chars += 1
+        df2 = df.merge(df_duplicate_shortnames, on='shortName', how='inner', suffixes=['', '_y'])
+        df2 = df2.drop(columns=['id_y'])
+        df2['shortName'] = (
+            df2['lastName'] 
+            + ' ' 
+            + df2['firstName'].apply(_abbreviate_name, args=[n_chars])
+        )
+
+        # Apply changes
+        df = df.merge(df2[['id', 'shortName']], on='id', how='left', suffixes=['', '_y'])
+        df['shortName'] = df['shortName_y'].fillna(df['shortName'])
+        df = df.drop(columns=['shortName_y'])
+
+        # Check duplicates
+        df_count = df.groupby('shortName')['id'].count()
+        df_duplicate_shortnames = df_count[df_count > 1]
+
+    return df
---
+++ tests/units/odds_test.py
@@ -0,0 +1,36 @@
+import pytest
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
+from tennis_predictor.helpers.odds import _abbreviate_name, make_shortname
+
+def test_abbreviate():
+    assert _abbreviate_name('Danilo') == 'D.'
+    assert _abbreviate_name('Danilo', n_chars=2) == 'Da.'
+    assert _abbreviate_name('Lee Sin') == 'L.S.'
+
+def test_make_shortname():
+    df_input = pd.DataFrame([
+        [1, 'Danilo', 'Acosta'],
+        [2, 'Dario', 'Acosta'],
+        [3, 'Diego', 'Acosta'],
+        [4, 'Abdul', 'Ahmed'],
+        [5, 'Jose', 'Antelo'],
+        [6, 'Jose', 'Antelo'],
+        [7, 'Seoung Hun', 'Lee'],
+    ],
+        columns = ['id', 'firstName', 'lastName']
+    )
+    df_output = pd.DataFrame([
+        [1, 'Danilo', 'Acosta', 'Acosta Dan.'],
+        [2, 'Dario', 'Acosta', 'Acosta Dar.'],
+        [3, 'Diego', 'Acosta', 'Acosta Di.'],
+        [4, 'Abdul', 'Ahmed', 'Ahmed A.'],
+        [5, 'Jose', 'Antelo', 'Antelo Jose.'],  # Same long shortname for same name
+        [6, 'Jose', 'Antelo', 'Antelo Jose.'],  # Same long shortname for same name
+        [7, 'Seoung Hun', 'Lee', 'Lee S.H.'],
+    ],
+        columns = ['id', 'firstName', 'lastName', 'shortName']
+    )
+    df_result = make_shortname(df_input)
+    assert_frame_equal(df_result, df_output)
